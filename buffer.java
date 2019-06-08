49a134eac7ba97fb89b848d30748f7973db62495
Arjunparama16 <46240299+Arjunparama16@users.noreply.github.com>
2019-06-08 13:00:17 +0530

    Add files via upload

---
+++ buffer.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class myfirstprogram
+
+{
+
+  public static void main(String args[])
+
+  {
+
+      int num1,num2,sum;
+
+      BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+      System.out.println("Ener the first number");
+     
+      num1=Integer.parseInt(br.readLine(System.in));
+  
+      System.out.println("Enter the second number");
+    
+      num2=Integer.parseInt(br.readLine(System.in));
+     
+      sum=num1+num2;
+   
+      System.out.println("The sum of two numbers:"+sum);
+ 
+  }
+
+}
